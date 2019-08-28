848126d49b4a6188350fe54bcc625595c8d92162
Studend@202-11 <Student@202-11>
2019-08-28 12:12:00 +0300

    Static

--- DataStructures/ForvardList/main.cpp
+++ DataStructures/ForvardList/main.cpp
@@ -8,26 +8,33 @@ class Element
 {
 	int Data;
 	Element* pNext;
+	static int count;
 public:
 	Element(int Data, Element* pNext = nullptr)
 	{
 		this->Data = Data;
 		this->pNext = pNext;
+		count++;
 		cout << "EConstructor:\t" << this << endl;
 	}
 	~Element()
 	{
+		count--;
 		cout << "Edestructor:\t" << this << endl;
 	}
 	friend class ForwardList;
 };
 
+int Element::count = 0;
+
 class ForwardList
 {
 	Element* Head;
+	unsigned int size;
 public:
 	ForwardList()
 	{
+		this->size = 0;
 		this->Head = nullptr;
 		cout << "LConstructor:\t"<< this << endl;
 	}
@@ -38,10 +45,10 @@ public:
 
 	void push_front(int Data)
 	{
-
 		Element* New = new Element(Data);
 		New->pNext = Head;
 		Head = New;
+		size++;
 	}
 
 	void print()
@@ -52,42 +59,72 @@ public:
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext; // переход на следующий елемент
 		}
+		cout << "Количество элементов списка: " << size << endl;
 	}
 
 	void push_back(int Data)
 	{
+		if (Head == nullptr)
+		{
+			push_front(Data);
+			return;
+		}
 		Element* Temp = Head;
 		while(Temp->pNext != nullptr)
 		{
 			Temp = Temp->pNext;
 		}
 		Temp->pNext = new Element(Data);
-		
+		size++;
+	} 
+
+	void insert(int index, int Data)
+	{
+		if (index > size)
+		{
+			cout << "Eror: Out of range." << endl;
+			return;
+		}
+		if (index == 0)
+		{
+			push_front(Data);
+			return;
+		}
+		Element* Temp = Head;
+		for (int i = 0; i < index-1; i++)
+		{
+			Temp = Temp->pNext;
+		}
+		Element* New = new Element(Data);
+		New->pNext = Temp->pNext;
+		Temp->pNext = New;
+		size++;
 	}
 
+	//Удаление элементов:
 	void pop_front()
 	{
 		Element* Temp = Head;
 		Temp = Temp->pNext;
 		delete Head;
 		Head = Temp;
+		size--;
 	}
 	void pop_back()
 	{
 		Element* Temp = Head;
-		while (Temp->pNext != nullptr)
+		while (Temp->pNext->pNext != nullptr)
 		{
 			Temp = Temp->pNext;
 		}
-		Temp->Data = NULL;
-		Temp = Head;
-		while (Temp->pNext->Data != NULL)
-		{
-			Temp = Temp->pNext;
-		}
+		delete Temp->pNext;
 		Temp->pNext = nullptr;
+		size--;
 	}
+
 };
+
+
 
 void main()
 {
@@ -111,4 +148,16 @@ void main()
 	fl.pop_back();
 	fl.print();
 	cout << delimiter;
+	int index;
+	int data;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "введите значение добавляемого элемента: "; cin >> data;
+	fl.insert(index, data);
+	fl.print();
+
+	ForwardList fl2;
+	fl2.push_back(3);
+	fl2.push_back(5);
+	fl2.push_back(8);
+	fl2.print();
 }
