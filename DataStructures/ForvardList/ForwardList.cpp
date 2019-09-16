4d6abdbc3cf939d56e689cb6f487c3620cd21408
Student <Student@304-1>
2019-09-16 18:33:31 +0300

    final

---
+++ DataStructures/ForvardList/ForwardList.cpp
@@ -0,0 +1,200 @@
+﻿#include "ForwardList.h"
+template <typename T>
+int ForwardList<T>::Element::count = 0;
+
+//template <typename T>
+template <typename T>
+ForwardList<T>::Element::Element(T Data, Element* pNext)
+{
+	this->Data = Data;
+	this->pNext = pNext;
+	count++;
+	cout << "EConstructor:\t" << this << endl;
+}
+template <typename T>
+ForwardList<T>::Element::~Element()
+{
+	count--;
+	cout << "EDestructor:\t" << this << endl;
+}
+
+template <typename T>
+ForwardList<T>::Iterator::Iterator(Element* Temp) :Temp(Temp)
+{
+	cout << "ItConstructor:\t" << this << endl;
+}
+template <typename T>
+ForwardList<T>::Iterator::~Iterator()
+{
+	cout << "ItDestructor:\t" << this << endl;
+}
+template <typename T>
+const T& ForwardList<T>::Iterator::operator*()const
+{
+	return Temp->Data;
+}
+template <typename T>
+T& ForwardList<T>::Iterator::operator*()
+{
+	return Temp->Data;
+}
+template <typename T>
+bool ForwardList<T>::Iterator::operator==(const Iterator& other)
+{
+	return this->Temp == other.Temp;
+}
+template <typename T>
+bool ForwardList<T>::Iterator::operator!=(const Iterator& other)
+{
+	return this->Temp != other.Temp;
+}
+
+//---------------------------------------------------------//
+template <typename T>
+const int ForwardList<T>::get_size()const
+{
+	return this->size;
+}
+template <typename T>
+ForwardList<T>::ForwardList()
+{
+	this->Head = nullptr;
+	this->size = 0;
+	cout << "LConstructor:\t" << this << endl;
+}
+template <typename T>
+ForwardList<T>::ForwardList(int size) :ForwardList()
+{
+	/*this->Head = nullptr;
+	this->size = 0;*/
+	while (size--)push_front(0);
+}
+template <typename T>
+ForwardList<T>::ForwardList(initializer_list<T> il) :ForwardList()
+{
+	cout << typeid(il.begin()).name() << endl;
+	for (T const* it = il.begin(); it != il.end(); it++)
+		push_back(*it);
+}
+template <typename T>
+ForwardList<T>::ForwardList(const ForwardList& other) :ForwardList()
+{
+	for (Element* Temp = other.Head; Temp != nullptr; Temp = Temp->pNext)
+		push_back(Temp->Data);
+	cout << "LCopyConstruct:\t" << this << endl;
+}
+template <typename T>
+ForwardList<T>::~ForwardList()
+{
+	while (Head)pop_front();
+	cout << "LDestructor:\t" << this << endl;
+}
+
+//			Operators:
+template <typename T>
+int& ForwardList<T>::operator[](int index)
+{
+	Element* Temp = Head;
+	for (int i = 0; i < index; i++)Temp = Temp->pNext;
+	return Temp->Data;
+}
+
+//			Äîáàâëåíèå ýëåìåíòîâ â ñïèñîê:
+template <typename T>
+void ForwardList<T>::push_front(T Data)
+{
+	//Data - äîáàâëÿåìîå çíà÷åíèå.
+	//1) Ñîçäàåì ýëåìåíò, â êîòîðûé ìîæíî ïîëîæèòü äîáàâëÿåìîå çíà÷åíèå:
+	/*Element* New = new Element(Data);
+	New->pNext = Head;
+	Head = New;*/
+	Head = new Element(Data, Head);
+	size++;
+}
+template <typename T>
+void ForwardList<T>::push_back(T Data)
+{
+	if (Head == nullptr)
+	{
+		push_front(Data);
+		return;
+	}
+	Element* Temp = Head;
+	while (Temp->pNext != nullptr)Temp = Temp->pNext;
+	Temp->pNext = new Element(Data);
+	size++;
+}
+template <typename T>
+void ForwardList<T>::insert(int index, T data)
+{
+	if (index == 0)
+	{
+		push_front(data);
+		return;
+	}
+	if (index > size)
+	{
+		cout << "Error: Out of range." << endl;
+		return;
+	}
+	Element* Temp = Head;
+	for (int i = 0; i < index - 1; i++)
+	{
+		Temp = Temp->pNext;
+	}
+	/*Element* New = new Element(data);
+	New->pNext = Temp->pNext;
+	Temp->pNext = New;*/
+	Temp->pNext = new Element(data, Temp->pNext);
+	size++;
+}
+
+//Óäàëåíèå ýëåìåíòîâ:
+template <typename T>
+void ForwardList<T>::pop_front()
+{
+	Element* buffer = Head;
+	Head = Head->pNext;
+	delete buffer;
+	size--;
+}
+template <typename T>
+void ForwardList<T>::pop_back()
+{
+	Element* Temp = Head;
+	while (Temp->pNext->pNext != nullptr)
+	{
+		Temp = Temp->pNext;
+	}
+	delete Temp->pNext;
+	Temp->pNext = nullptr;
+	size--;
+}
+template <typename T>
+void ForwardList<T>::erase(int index)
+{
+	if (index == 0)
+	{
+		pop_front();
+		return;
+	}
+	if (index >= size)
+	{
+		cout << "Error: out of range" << endl;
+		return;
+	}
+	Element* Temp = Head;
+	for (int i = 0; i < index - 1; i++)Temp = Temp->pNext;
+	Element* buffer = Temp->pNext;
+
+	Temp->pNext = Temp->pNext->pNext;
+	delete buffer;
+	size--;
+}
+template <typename T>
+void ForwardList<T>::print()
+{
+	for (Element* Temp = Head; Temp != nullptr; Temp = Temp->pNext)
+		cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+	cout << "Длинна списка: " << size << endl;
+}
---
+++ DataStructures/ForvardList/ForwardList.h
@@ -0,0 +1,112 @@
+﻿#pragma once
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+//#define BASE_CHECK
+//#define INDEX_OPERATOR_CHECK
+//#define CONSTRUCTORS_CHECK
+
+
+template <typename T>
+class ForwardList
+{
+	class Element
+	{
+		T Data;		//Çíà÷åíèå ýëåìåíòà
+		Element* pNext;	//Àäðåñ ñëåäóþùåãî ýëåìåíòà
+		static int count;
+	public:
+		Element(T Data, Element* pNext = nullptr);
+		~Element();
+		friend class ForwardList;
+	};
+	Element* Head;	//Óêàçàòåëü íà íà÷àëî ñïèñêà
+	unsigned int size;
+public:
+	class Iterator
+	{
+		Element* Temp;
+	public:
+		Iterator(Element* Temp = nullptr);
+		~Iterator();
+		Iterator& operator++()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		Iterator operator++(int)
+		{
+			Iterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+		const T& operator*()const;
+		T& operator*();
+		bool operator==(const Iterator& other);
+		bool operator!=(const Iterator& other);
+	};
+	const int get_size()const;
+
+	const Iterator begin()const
+	{
+		return Head;
+	}
+	Iterator begin()
+	{
+		return Head;
+	}
+
+	const Iterator end() const
+	{
+		return nullptr;
+	}
+	Iterator end()
+	{
+		return nullptr;
+	}
+
+	ForwardList();
+	ForwardList(int size);
+	ForwardList(initializer_list<T> il);
+	ForwardList(const ForwardList& other);
+	~ForwardList();
+
+	//			Operators:
+	ForwardList& operator=(const ForwardList& other)
+	{
+		if (this == &other)return *this;
+		while (Head)pop_front();
+		for (Element* Temp = other.Head; Temp != nullptr; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		cout << "LCopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	int& operator[](int index);
+
+	//			Äîáàâëåíèå ýëåìåíòîâ â ñïèñîê:
+	void push_front(T Data);
+	void push_back(T Data);
+
+	void insert(int index, T data);
+
+	//Óäàëåíèå ýëåìåíòîâ:
+	void pop_front();
+
+	void pop_back();
+
+	void erase(int index);
+
+	void print();
+	friend ForwardList<T> operator+(const ForwardList<T>& left, const ForwardList<T>& right)
+	{
+		ForwardList<T> cat = left;
+		for (ForwardList<T>::Iterator it = right.begin(); it != right.end(); it++)
+			cat.push_back(*it);
+		return cat;
+	}
+};
