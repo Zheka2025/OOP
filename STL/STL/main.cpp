c4633bc84d41422b65a1d75a06dd22ca9c6fcb3b
Student <Student@304-1>
2019-09-19 21:01:27 +0300

    STL

---
+++ STL/STL/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<array>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int Arr[] = { 3, 5, 8, 13, 21 };
+	array<int, 5> arr = { 3, 5, 8, 13, 21 };
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
